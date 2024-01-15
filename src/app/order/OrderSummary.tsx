39b07cf21c8fd10c89e3566e1e34af2cd33befb2
jsh3418 <albert5428@gmail.com>
2024-01-15 20:32:44 +0900

    refactor: OrderSummary 스타일 상수 적용

--- src/app/order/OrderSummary.tsx
+++ src/app/order/OrderSummary.tsx
@@ -1,9 +1,10 @@
+import { TYPOGRAPHY } from "@/constants/typography";
 import OrderButton from "./OrderButton";
 
 export default function OrderSummary() {
   return (
     <div className="w-full rounded-t-[20px] bg-white absolute bottom-0 shadow-[0_-4px_10px_0_#00000040] p-[23px_27px_27px_27px]">
-      <div className="mb-[18px] font-inter text-[18px] font-normal leading-[22px] text-right tracking-normal">
+      <div className={`mb-[18px] text-right ${TYPOGRAPHY.NORMAL}`}>
         <div>총 수량 : 0원</div>
         <div>총 가격 : 0원</div>
       </div>
