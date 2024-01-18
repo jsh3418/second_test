dcd77628ba2560026bfdcbd12b15097beeeeaa2f
jsh3418 <albert5428@gmail.com>
2024-01-18 20:35:20 +0900

    feat: 주문버튼 컴포넌트 resetCart 추가 및 로직 개선

--- src/app/order/OrderButton.tsx
+++ src/app/order/OrderButton.tsx
@@ -1,6 +1,8 @@
 import { PATH } from "@/constants/path";
+import { getSuccessOrFailure } from "@/utils/getSuccessOrFailure";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useCartStore } from "./useCartStore";
 
 type Props = {
   disabled: boolean;
@@ -10,25 +12,42 @@ export default function OrderButton({ disabled }: Props) {
   const router = useRouter();
 
   const [isLoading, setIsLoading] = useState(false);
+  const resetCart = useCartStore(({ resetCart }) => resetCart);
 
-  const fakeOrder = () => {
+  const onSuccess = () => {
+    resetCart();
+    router.push(PATH.COMPLETE);
+  };
+
+  const onFailure = () => {
+    router.push(PATH.ERROR);
+  };
+
+  const onOrderButtonClick = () => {
+    if (disabled) return;
+
     setIsLoading(true);
+
+    const result = getSuccessOrFailure();
+
     setTimeout(() => {
-      if (Math.random() > 0.5) {
-        router.push(PATH.COMPLETE);
+      if (result === "success") {
+        onSuccess();
       } else {
-        router.push(PATH.ERROR);
+        onFailure();
       }
     }, 2000);
   };
 
+  const orderButtonDisabled = disabled || isLoading;
+
   return (
     <button
       className={`w-full h-[48px] text-white${
-        disabled || isLoading ? " bg-argent" : " bg-black"
+        orderButtonDisabled ? " bg-argent" : " bg-black"
       }`}
-      disabled={disabled || isLoading}
-      onClick={fakeOrder}
+      disabled={orderButtonDisabled}
+      onClick={onOrderButtonClick}
     >
       {isLoading ? "로딩중..." : "주문하기"}
     </button>
